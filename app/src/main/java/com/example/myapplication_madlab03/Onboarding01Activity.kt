c4142945db5d30c14bf56fbed9eb3bc5ef2e0ec9
Bineth <binethellepola@gmail.com>
2025-09-23 22:49:18 +0530

    added navigations

--- app/src/main/java/com/example/myapplication_madlab03/Onboarding01Activity.kt
+++ app/src/main/java/com/example/myapplication_madlab03/Onboarding01Activity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication_madlab03
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,13 +14,28 @@ class Onboarding01Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding_01)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        // Find views
+        val nextButton: Button = findViewById(R.id.nextButton)
+        val skipText: TextView = findViewById(R.id.skipText)
 
+        // Navigate to Onboarding 2
+        nextButton.setOnClickListener {
+            val intent = Intent(this, Onboarding02Activity::class.java)
+            startActivity(intent)
+        }
 
+        // Skip to Home
+        skipText.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // so user can’t go back to onboarding
+        }
     }
 }
